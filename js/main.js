f691c9c36ed7d5deb2d6684b4f90b446ad424254
OlexandrYaremchuk <70215770+OlexandrYaremchuk@users.noreply.github.com>
2023-12-25 20:55:07 +0200

    start project

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// призначаю групу
+const box = document.querySelector(".js_box");
+let grNumber;
+function getNumberGr() {
+  const max = 2;
+  const min = 1;
+  grNumber = Math.round(Math.random() * (max - min) + min);
+  // Повертаємо 1 або 2 в залежності від випадкового значення
+  return grNumber;
+}
+
+function addGroup() {
+  getNumberGr();
+  box.innerHTML = `<div class="group">${grNumber}</div>`;
+}
+setTimeout(addGroup, 3000);

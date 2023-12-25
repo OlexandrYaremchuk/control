495bea197a112b3212eee6531b156ee6452233c5
OlexandrYaremchuk <70215770+OlexandrYaremchuk@users.noreply.github.com>
2023-12-25 23:51:07 +0200

    task1

--- js/main.js
+++ js/main.js
@@ -17,7 +17,25 @@ setTimeout(addGroup, 3000);
 
 box.addEventListener("touchstart", (e) => {
   box.innerHTML = `<div class="group">
-  <p></p>
+  <p class="question_text">У веб-просторах та веб-краях, Є місце, де дані зачаєно, як скарб.<br/> 
+  Відкрий інструменти, поглянь на "аплікуху". <br/>
+   Кожні 5 секунд то прийде то зникне, як таємничий герой.<br/>
+  
+  Та завжди пам'ятайте  - це не особистий бенкет, тут жодних персональних слів, лише код та пакет.</p>
    <input type="text" class="qestions" placeholder=""sdsdsd/>
+   <div class="feedback"></div>
    </div> `;
+  const getInputValue = document.querySelector(".qestions");
+  console.log(getInputValue);
+  getInputValue.addEventListener("change", (e) => {
+    let value = e.currentTarget.value;
+    console.log(value);
+    const feedback = document.querySelector(".feedback");
+    if (value.toLowerCase() === "local storage") {
+      feedback.innerHTML = `<img src="https://cdn-icons-png.flaticon.com/512/7778/7778643.png" width="50" height="50" />`;
+      localStorage.setItem("value1", JSON.stringify(value));
+    } else {
+      feedback.innerHTML = `<img src="https://www.freeiconspng.com/thumbs/error-icon/error-icon-4.png" width="50" height="50" />`;
+    }
+  });
 });
